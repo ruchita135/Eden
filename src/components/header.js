c0178d3ba26cab40801eafc329735d83c8a2e6c9
Ruchita Singh <ruchitasingh13@gmail.com>
2022-10-05 15:49:06 +0530

    added components page

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img alt="Logo" src={logo}/>
+        </header>
+      </div>
+    );
+  }
+
+  export default Header;
